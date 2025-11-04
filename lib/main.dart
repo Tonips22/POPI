7e26110467bebe946fe371078d7969cab4279eda
Jose <josemanu408@gmail.com>
2025-11-04 20:31:46 +0100

    Pantalla de selección de juego

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'screens/number_screen.dart';
+import 'screens/game_selector_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const NumberScreen(),
+      home: const ChooseGameScreen(),
     );
   }
 }
