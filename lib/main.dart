f4615cfc007d08f054e5a7043e08bdcf7bf0c098
Jose <josemanu408@gmail.com>
2025-11-05 13:59:01 +0100

    Crear perfil de alumno funcional

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'screens/number_screen.dart';
 import 'screens/game_selector_screen.dart';
+import 'screens/customization_screen.dart';
+import 'screens/fonts_settings_screen.dart';
+import 'screens/number_format_screen.dart';
+import 'screens/color_settings_screen.dart';
+import 'screens/create_profile_screen.dart';
 
 void main() {
   runApp(const MyApp());
