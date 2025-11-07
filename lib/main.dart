0e8c543f4763751bb85dffd5d93fcc6d4c482c40
Tonips22 <130395782+Tonips22@users.noreply.github.com>
2025-11-07 09:27:47 +0100

    refactor: Eliminar importaciones no utilizadas en main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,4 @@
 import 'package:flutter/material.dart';
-import 'screens/number_screen.dart';
-import 'screens/game_selector_screen.dart';
-import 'screens/customization_screen.dart';
-import 'screens/fonts_settings_screen.dart';
-import 'screens/number_format_screen.dart';
-import 'screens/color_settings_screen.dart';
-import 'screens/create_profile_screen.dart';
 import 'screens/login_screen.dart';
 
 void main() {
